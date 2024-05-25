071efdeb79ec34bb6ff75b27f1e9f7601cbedda5
Duarte6467 <111036625+Duarte6467@users.noreply.github.com>
2024-05-25 21:54:14 +0100

    1.0

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
-from accelerate import Accelerator
+import pdfplumber
 
-# Initialize the Accelerator
-accelerator = Accelerator()
 
-# Load the model and tokenizer
-tokenizer = AutoTokenizer.from_pretrained("AIFS/Prometh-222B")
-model = AutoModelForCausalLM.from_pretrained("AIFS/Prometh-222B")
+pdf = pdfplumber.open('CRPD.pdf')
 
-# Tell Accelerate to prepare the model
-model, tokenizer = accelerator.prepare(model, tokenizer)
 
-# Prepare your text data
-prompt = "The future of AI in healthcare is"
-input_ids = tokenizer(prompt, return_tensors="pt", padding=True, truncation=True, max_length=512)
+# Extract text from the first page
+first_page = pdf.pages[0]
+text =''
 
-# Use the model and tokenizer to generate text
-# Note: We directly use model and tokenizer without pipeline for better control and efficiency in multi-GPU setups.
-generated_ids = accelerator.unwrap_model(model).generate(**input_ids, max_length=50, num_return_sequences=3)
 
-# Decode generated ids to text
-for g in generated_ids:
-    print(tokenizer.decode(g, skip_special_tokens=True))
+for page in pdf.pages:
+    text += page.extract_text()
+
+"""Text is now processed into a string"""
+
